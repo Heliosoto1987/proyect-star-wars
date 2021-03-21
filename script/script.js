1c648b25592b723e7821b177ea3af733c57cfaf5
Helio <heliosoto17@gmail.com>
2021-03-22 00:29:37 +0100

    new texts

--- script/script.js
+++ script/script.js
@@ -47,7 +47,7 @@ const arrayphotos = [
     function menu2(menu) {
         menu.classList.toggle('lol')
             if(menu.classList.contains('lol')){
-            menu.innerHTML += `<span class="menu2__span">Lord Venao</span>` + '<span class="menu2__span">Perfect Jedi</span>' + '<span class="menu2__span">Momy Joda</span>' + '<span class="menu2__span">Dark senior</span>' + '<span class="menu2__span">Padawan</span>' +'<span class="menu2__span">Er papa de los Helados</span>'
+            menu.innerHTML += `<span class="menu2__span">Lord Venao</span>` + '<span class="menu2__span">Perfect Jedi</span>' + '<span class="menu2__span">Momy Joda</span>' + '<span class="menu2__span">Dark senior</span>' + '<span class="menu2__span">Padawan</span>' +'<span class="menu2__span">El campeon</span>'
             }else{
             menu.innerHTML = 'Divertidos'
             } 
@@ -67,17 +67,17 @@ const arrayphotos = [
     function activate2(event) {
         const select = event.target.innerHTML;
             if (select === 'Lord Venao') {
-                reciver.innerHTML = `<div class="name">Nombre: Lord Venao</div>` + `<div class="height">Altura: 1.69</div>` + `<div class="mass">Peso: Un verguero</div>` + `<div class="img"><img class="img__inside" src="img/papa123.jpg" alt="Disculpe ha ocurrido un error"></div>` + `<div class="reciver__text">Este terrible Jedi es un peligro para las reservas de azucar en el mundo</div>`;
+                reciver.innerHTML = `<div class="name">Nombre: Lord Venao</div>` + `<div class="height">Altura: 1.69</div>` + `<div class="mass">Peso: Incalculable</div>` + `<div class="img"><img class="img__inside" src="img/papa123.jpg" alt="Disculpe ha ocurrido un error"></div>` + `<div class="reciver__text">Este terrible Jedi es un peligro para las reservas de azucar en el mundo</div>`;
             }else if (select === 'Momy Joda') {
                 reciver.innerHTML = `<div class="name">Nombre: Momy Joda</div>` + `<div class="height">Altura: 1.60</div>` + `<div class="mass">Peso: 43Kg?xD</div>` + `<div class="img"><img class="img__inside" src="img/momy.jpg" alt="Disculpe ha ocurrido un error"></div>` + `<div class="reciver__text">Maestra jedi, la mas hermosa de todas</div>`;
             }else if (select === 'Padawan') {
-                reciver.innerHTML = `<div class="name">Nombre: Hermano querido</div>` + `<div class="height">Altura: 1.75</div>` + `<div class="mass">Peso: No sale de 75Kg</div>` + `<div class="img"><img class="img__inside" src="img/padawan.jpg" alt="Disculpe ha ocurrido un error"></div>` + `<div class="reciver__text">Esta mas rezagao que el Huesca</div>`;
+                reciver.innerHTML = `<div class="name">Nombre: Hermano querido</div>` + `<div class="height">Altura: 1.75</div>` + `<div class="mass">Peso: No sale de 75Kg</div>` + `<div class="img"><img class="img__inside" src="img/padawan.jpg" alt="Disculpe ha ocurrido un error"></div>` + `<div class="reciver__text">Buscando la felicidad</div>`;
             }else if (select === 'Dark senior') {
-                reciver.innerHTML = `<div class="name">Nombre: Dark Senior</div>` + `<div class="height">Altura: 2.03</div>` + `<div class="mass">Peso: 145Kg</div>` + `<div class="img"><img class="img__inside" src="img/ale.jpg" alt="Disculpe ha ocurrido un error"></div>` + `<div class="reciver__text">Pontente Senior Front End... no te pido mucho pero... DAME TRABAJO :D</div>`;
+                reciver.innerHTML = `<div class="name">Nombre: Dark Senior</div>` + `<div class="height">Altura: 2.03</div>` + `<div class="mass">Peso: 145Kg</div>` + `<div class="img"><img class="img__inside" src="img/ale.jpg" alt="Disculpe ha ocurrido un error"></div>` + `<div class="reciver__text">Pontente Senior Front End... </div>`;
             }else if (select === 'Perfect Jedi') {
                 reciver.innerHTML = `<div class="name">Nombre: Jedi Perfecta</div>` + `<div class="height">Altura: 1.72</div>` + `<div class="mass">Peso: 42</div>` + `<div class="img"><img class="img__inside" src="img/neidy.jpg" alt="Disculpe ha ocurrido un error"></div>` + `<div class="reciver__text">Estos datos han sido manipulados ALERT!!!</div>`;
-            }else if (select === 'Er papa de los Helados') {
-                reciver.innerHTML = `<div class="name">Nombre: Vergatarius Jedi Master</div>` + `<div class="height">Altura: 2.15</div>` + `<div class="mass">Peso: 90Kg</div>` + `<div class="img"><img class="img__inside" src="img/helio.jpg" alt="Disculpe ha ocurrido un error"></div>` + `<div class="reciver__text">Este es el campeon de la galaxia si lo ves corre mi hermano</div>`;
+            }else if (select === 'El campeon') {
+                reciver.innerHTML = `<div class="name">Nombre: Jedi Front End</div>` + `<div class="height">Altura: 1.85</div>` + `<div class="mass">Peso: 115kg</div>` + `<div class="img"><img class="img__inside" src="img/helio.jpg" alt="Disculpe ha ocurrido un error"></div>` + `<div class="reciver__text">Campeon de la galaxia</div>`;
             }else if(select === 'Divertidos') {
             reciver.innerHTML = ''
             }
